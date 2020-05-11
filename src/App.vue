6d263ea214fdde7596f28fa0cdf19fc197fcc45b
krave1986 <krave@qq.com>
2020-05-11 13:04:41 +0800

    问题还很多

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,47 @@
 		<div class="output">
 			<img id="photo" alt="The screen capture will appear in this box." />
 		</div>
+		<div>{{ ua }}</div>
 	</div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
 	name: "app",
 	methods: {},
-	computed: {}
+	computed: {
+		ua() {
+			return navigator.userAgent.toLowerCase();
+		}
+	},
+	mounted() {
+		var width = 320; // 把照片的宽度缩放到这么大
+		var height = 0; // 这个会根据 input stream 进行计算
+
+		var streaming = false;
+
+		var video = null;
+		var canvas = null;
+		var photo = null;
+		var startbutton = null;
+
+		video = document.getElementById("video");
+		canvas = document.getElementById("canvas");
+		photo = document.getElementById("photo");
+		startbutton = document.getElementById("startbutton");
+
+		// 获取 media stream
+		navigator.mediaDevices
+			.getUserMedia({ video: true, audio: false })
+			.then(function(stream) {
+				video.srcObject = stream;
+				// video.play();
+			})
+			.catch(function(err) {
+				console.log("An error occurred: " + err);
+			});
+	}
 };
 </script>
 
@@ -25,6 +58,16 @@ body {
 	display: grid;
 	justify-content: center;
 	align-items: center;
+	height: 100vh;
+}
+#video {
+	transform: scaleX(-1);
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	width: 100vw;
 	height: 100vh;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
 	devServer: {
-		host: "h5.wulicoco.online",
+		// host: "h5.wulicoco.online",
 		https: true,
 		key: fs.readFileSync(path.resolve(__dirname, "./2_h5.wulicoco.online.key")),
 		cert: fs.readFileSync(path.resolve(__dirname, "./1_h5.wulicoco.online_bundle.crt"))
